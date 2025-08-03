2deb0365cf3ca0eb7965b533beed98e65734ed87
Lesetja <kmalapane155@gmail.com>
2025-08-03 09:49:02 +0200

    added ToDo class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log("Hello, world");
+
+class ToDo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
+
+const les = new ToDo("les", "hello World", new Date(), "top");
+console.log(les);
