000287a2900989f3303f2e7dcf52d34333e8b239
Lesetja <kmalapane155@gmail.com>
2025-08-10 00:29:05 +0200

    added dom elements and separated/changed the names/id of elements

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {createList, createTask} from "./functions"
+import { createList, createTask } from "./functions"
 import "./styles.css"
 
 const tasks = createList("primary");
@@ -8,5 +8,25 @@ const kat = createTask("Story", "My Life", new Date(), "top");
 
 tasks.addItem(les);
 tasks.addItem(kat);
+
+
+const taskButton = document.getElementById("createNewPage");
+const taskSideBar = document.getElementById("pages");
+
+taskButton.addEventListener("click", () => {
+    const formContent = document.createElement("form");
+    const title = document.createElement("input");
+    const addPageButton = document.createElement("button");
+
+    addPageButton.textContent = "Add";
+    addPageButton.id = "addPageButton";
+    title.id = "title";
+    title.name = "title";
+    title.type = "text";
+
+    formContent.appendChild(title)
+    formContent.appendChild(addPageButton)
+    taskSideBar.appendChild(formContent)
+})
 
 console.log(tasks.getList());
