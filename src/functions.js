fdab93cb44b590eada43fad45c1dbf7eb7540d37
Lesetja <lemalapjhb024@student.wethinkcode.co.za>
2025-08-11 17:47:00 +0200

    errors

--- src/functions.js
+++ src/functions.js
@@ -9,4 +9,33 @@ function createTask(title, description, dueDate, priority) {
         title, description, dueDate, priority
     )
 }
-export {createList, createTask}
+
+function showForm(contentDiv) {
+    const formContent = document.createElement("form");
+
+    // event.preventDefault();
+
+    const title = document.createElement("input");
+    const addPageButton = document.createElement("input");
+    addPageButton.type = "submit";
+
+    // addPageButton.textContent = "Add";
+    addPageButton.id = "addPageButton";
+    title.id = "title";
+    title.name = "title";
+    title.type = "text";
+
+    formContent.appendChild(title)
+    formContent.appendChild(addPageButton)
+    contentDiv.appendChild(formContent)
+
+    return formContent.elements["title"].value;
+}
+
+function createPage(title) {
+    const dfsg = document.createElement("h1");
+    dfsg.textContent(title)
+    document.body.appendChild(dfsg)
+}
+
+export {createList, createTask, showForm, createPage}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import { createList, createTask } from "./functions"
-import "./styles.css"
+import { createList, createTask, showForm, createPage } from "./functions";
+import "./styles.css";
 
 const tasks = createList("primary");
+let title;
 
 const les = createTask("les", "hello World", new Date(), "top");
 const kat = createTask("Story", "My Life", new Date(), "top");
@@ -9,24 +10,12 @@ const kat = createTask("Story", "My Life", new Date(), "top");
 tasks.addItem(les);
 tasks.addItem(kat);
 
-
 const taskButton = document.getElementById("createNewPage");
 const taskSideBar = document.getElementById("pages");
 
 taskButton.addEventListener("click", () => {
-    const formContent = document.createElement("form");
-    const title = document.createElement("input");
-    const addPageButton = document.createElement("button");
-
-    addPageButton.textContent = "Add";
-    addPageButton.id = "addPageButton";
-    title.id = "title";
-    title.name = "title";
-    title.type = "text";
-
-    formContent.appendChild(title)
-    formContent.appendChild(addPageButton)
-    taskSideBar.appendChild(formContent)
-})
+    title = showForm(taskSideBar);
+    createPage(title)
+});
 
 console.log(tasks.getList());
